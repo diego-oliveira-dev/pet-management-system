a3092e59d98b9ff52f88ddcee46d72aa21314ab9
Diego <dglvr3@gmail.com>
2026-05-02 12:05:23 -0300

    feat: criação da estrutura básica do menu e validação de entrada

---
+++ src/Validador.java
@@ -0,0 +1,17 @@
+public class Validador {
+    public static boolean isEscolhaValida(String escolha) {
+        int escolhaInt = 0;
+        try {
+            escolhaInt = Integer.parseInt(escolha);
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (escolhaInt < 1 || escolhaInt > 6) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
