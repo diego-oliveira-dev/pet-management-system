f81cc788dbb81d75d25b1ec689ed9f9de6f917ca
Diego <dglvr3@gmail.com>
2026-06-19 08:53:55 -0300

    refactor: update owner entity for simplified manipulation

--- src/main/java/com/projetos/diego/pet_management_system/domain/PetOwner.java
+++ src/main/java/com/projetos/diego/pet_management_system/domain/PetOwner.java
@@ -2,11 +2,7 @@ package com.projetos.diego.pet_management_system.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -15,7 +11,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PetOwner implements UserDetails {
+public class PetOwner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,36 +19,6 @@ public class PetOwner implements UserDetails {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private UserRole role;
-
     @OneToMany(mappedBy = "petOwner")
     private List<Pet> pets;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.role.equals(UserRole.ADMIN)) {
-            return List.of(
-                    new SimpleGrantedAuthority("ROLE_USER"),
-                    new SimpleGrantedAuthority("ROLE_ADMIN")
-            );
-        }
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Getter
-    @AllArgsConstructor
-    public enum UserRole {
-        USER("user"),
-        ADMIN("admin");
-
-        private final String role;
-    }
 }
---
+++ src/main/java/com/projetos/diego/pet_management_system/dto/PetPostRequest.java
@@ -0,0 +1,72 @@
+package com.projetos.diego.pet_management_system.requests;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projetos.diego.pet_management_system.domain.Pet;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class PetPostRequestBody {
+    @NotBlank(message = "Pet name cannot be empty or blank")
+    @Schema(description = "This is the name of the pet.",
+            example = "Rex",
+            requiredMode = Schema.RequiredMode.REQUIRED)
+    private String name;
+
+    @NotNull(message = "Pet type cannot be null")
+    @Schema(description = "This is the type of the pet. Allowed values are DOG and CAT.",
+            requiredMode = Schema.RequiredMode.REQUIRED)
+    private Pet.Type type;
+
+    @NotNull(message = "Pet sex cannot be null")
+    @Schema(description = "This is the type of the pet. Allowed values are MALE and FEMALE.",
+            requiredMode = Schema.RequiredMode.REQUIRED)
+    private Pet.Sex sex;
+
+    @PastOrPresent(message = "Pet birth date cannot be a future date")
+    @NotNull(message = "Pet birth date cannot be null")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @Schema(description = "This is the birth date of the pet. Valid date format: yyyy-MM-dd.",
+            example = "2022-03-10",
+            requiredMode = Schema.RequiredMode.REQUIRED)
+    private LocalDate birthDate;
+
+    @NotNull(message = "Pet weight cannot be empty")
+    @DecimalMin(value = "0.5", message = "Pet weight must be greater than or equal to 0.5")
+    @DecimalMax(value = "150.0", message = "Pet weight must be less than or equal to 150.0")
+    @Schema(description = "This is the weight of the pet. Must be a number between 0.5 and 150.0 inclusive.",
+            example = "95.3",
+            requiredMode = Schema.RequiredMode.REQUIRED)
+    private Double weight;
+
+    @Pattern(regexp = ".*\\S+.*", message = "Pet breed cannot be empty or blank")
+    @Schema(description = "This is the breed of the pet.",
+            example = "Golden Retriever",
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+            nullable = true)
+    private String breed;
+
+    @Pattern(regexp = "^\\d{8}$", message = "Postal code must be exactly 8 digits long")
+    @Pattern(regexp = ".*\\S+.*", message = "Postal code cannot be empty or blank")
+    @Schema(description = "This is the postal code of the owner. Must be exactly 8 digits long without empty spaces",
+            example = "64009100",
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+            nullable = true)
+    private String postalCode;
+
+    @NotBlank(message = "Pet owner name cannot be empty or blank")
+    @Pattern(regexp = "^$|\\s*\\S+.*", message = "Field must not be empty or blank")
+    @Schema(description = "This is the pet's owner name.",
+            example = "Diego Oliveira",
+            requiredMode = Schema.RequiredMode.REQUIRED)
+    private String owner;
+}
