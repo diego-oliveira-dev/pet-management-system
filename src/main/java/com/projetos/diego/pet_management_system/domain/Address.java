5bd834c73b33e4bcc124b1cb56df0059cf8a7b8a
Diego <dglvr3@gmail.com>
2026-06-20 15:50:20 -0300

    refactor: introduce embeddable Address and refactor AddressLookupService and its implementations

---
+++ src/main/java/com/projetos/diego/pet_management_system/domain/Address.java
@@ -0,0 +1,30 @@
+package com.projetos.diego.pet_management_system.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Address {
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String neighbourhood;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+}
