12bb998eeaa551fcc7b11e92621af3046e728187
Diego <dglvr3@gmail.com>
2026-06-25 13:51:09 -0300

    test: add web and service layers tests for '/auth/login' endpoint

---
+++ src/test/java/com/projetos/diego/pet_management_system/controller/AuthenticationControllerTest.java
@@ -0,0 +1,94 @@
+package com.projetos.diego.pet_management_system.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projetos.diego.pet_management_system.config.SecurityConfig;
+import com.projetos.diego.pet_management_system.dto.AuthenticationRequest;
+import com.projetos.diego.pet_management_system.dto.AuthenticationResponse;
+import com.projetos.diego.pet_management_system.exception.InvalidCredentialsException;
+import com.projetos.diego.pet_management_system.service.AuthenticationService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(AuthenticationController.class)
+@Import(SecurityConfig.class)
+class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private AuthenticationService authenticationServiceMock;
+
+    @Test
+    @DisplayName("login returns 200 when successful")
+    void login_Returns200_WhenSuccessful() throws Exception {
+        AuthenticationRequest request = AuthenticationRequest.builder()
+                .username("diego123")
+                .password("secret123")
+                .build();
+        AuthenticationResponse response = AuthenticationResponse.builder()
+                .token("token-value")
+                .type("Bearer")
+                .build();
+
+        BDDMockito.when(authenticationServiceMock.login(request))
+                        .thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.token")
+                        .value(response.token()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.type")
+                        .value(response.type()));
+    }
+
+    @Test
+    @DisplayName("login returns 400 when credentials are invalid")
+    void login_Returns400_WhenCredentialsAreInvalid() throws Exception {
+        AuthenticationRequest request = AuthenticationRequest.builder()
+                .username("")
+                .password(null)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title")
+                        .value("Validation Failed"));;
+    }
+
+    @Test
+    @DisplayName("login returns 401 when user is not found or password is incorrect")
+    void login_Returns401_WhenUserIsNotFoundOrPasswordIsIncorrect() throws Exception {
+        AuthenticationRequest request = AuthenticationRequest.builder()
+                .username("invalid")
+                .password("not a password")
+                .build();
+
+        BDDMockito.when(authenticationServiceMock.login(request))
+                .thenThrow(new InvalidCredentialsException("Invalid username or password"));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+}
---
+++ src/test/java/com/projetos/diego/pet_management_system/service/AuthenticationServiceTest.java
@@ -0,0 +1,81 @@
+package com.projetos.diego.pet_management_system.service;
+
+import com.projetos.diego.pet_management_system.dto.AuthenticationRequest;
+import com.projetos.diego.pet_management_system.dto.AuthenticationResponse;
+import com.projetos.diego.pet_management_system.security.JwtService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@ExtendWith(MockitoExtension.class)
+class AuthenticationServiceTest {
+
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    @Mock
+    private JwtService jwtServiceMock;
+
+    @Mock
+    private AuthenticationManager authenticationManagerMock;
+
+    @Test
+    @DisplayName("login returns token when credentials are valid")
+    void login_ReturnsToken_WhenCredentialsAreValid() {
+        AuthenticationRequest request = AuthenticationRequest.builder()
+                .username("diego123")
+                .password("secret123")
+                .build();
+        Authentication authentication = Mockito.mock(Authentication.class);
+        BDDMockito.when(authenticationManagerMock.authenticate(ArgumentMatchers.any()))
+                .thenReturn(authentication);
+        BDDMockito.when(jwtServiceMock.generateToken(authentication))
+                .thenReturn("jwt-token");
+
+        AuthenticationResponse response = authenticationService.login(request);
+
+        Assertions.assertThat("jwt-token").isEqualTo(response.token());
+        Assertions.assertThat("Bearer").isEqualTo(response.type());
+        Mockito.verify(jwtServiceMock, Mockito.times(1))
+                .generateToken(authentication);
+    }
+
+    @Test
+    @DisplayName("login throws UsernameNotFoundException when user does not exist")
+    void login_ThrowsUsernameNotFoundException_WhenUserDoesNotExist() {
+        AuthenticationRequest request = AuthenticationRequest.builder()
+                .username("invalid")
+                .password("password")
+                .build();
+        BDDMockito.when(authenticationManagerMock.authenticate(ArgumentMatchers.any()))
+                .thenThrow(new UsernameNotFoundException("Owner not found with username 'invalid'"));
+
+        Assertions.assertThatExceptionOfType(UsernameNotFoundException.class)
+                .isThrownBy(() -> this.authenticationService.login(request))
+                .withMessageContaining("Owner not found with username 'invalid'");
+        Mockito.verify(jwtServiceMock, Mockito.never()).generateToken(ArgumentMatchers.any());
+    }
+
+    @Test
+    @DisplayName("login throws BadCredentialsException when password is invalid")
+    void login_ThrowsBadCredentialsException_WhenPasswordIsInvalid() {
+        AuthenticationRequest request = AuthenticationRequest.builder()
+                .username("diego123")
+                .password("wrong")
+                .build();
+        BDDMockito.when(authenticationManagerMock.authenticate(ArgumentMatchers.any()))
+                .thenThrow(new BadCredentialsException("Invalid credentials"));
+
+        Assertions.assertThatExceptionOfType(BadCredentialsException.class)
+                .isThrownBy(() -> this.authenticationService.login(request))
+                .withMessageContaining("Invalid credentials");
+        Mockito.verify(jwtServiceMock, Mockito.never()).generateToken(ArgumentMatchers.any());
+    }
+}
